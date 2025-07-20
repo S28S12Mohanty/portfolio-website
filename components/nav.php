5e51e73ac82c0d99acf870056ae93e009e7e9cdf
SUNAMI SWAYAMPRAVA MOHANTY <mohantysunamiswayamprava@gmail.com>
2025-07-21 00:20:03 +0530

    Update project section

--- components/nav.php
+++ components/nav.php
@@ -2,7 +2,7 @@
     <ul class="nav-menu">
         <li><a href="index.php" class="nav-link active" title="About"><i class="fas fa-user-astronaut"></i></a></li>
         <li><a href="skills.php" class="nav-link" title="Skill"><i class="fas fa-rocket"></i></a></li>
-        <li><a href="project.html" class="nav-link" title="Project"><i class="fas fa-cubes"></i></a></li>
+        <li><a href="project.php" class="nav-link" title="Project"><i class="fas fa-cubes"></i></a></li>
         <li><a href="experience.html" class="nav-link" title="Experience"><i class="fas fa-mountain"></i></a></li>
         <li><a href="#contact" class="nav-link" title="Contact"><i class="fas fa-envelope"></i></a></li>
     </ul>
